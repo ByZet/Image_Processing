2f3cf53a4d58c5a50631e8c8397482dc7c1f00b5
KADİR CAN KIZILARSLAN <55657487+ByZet@users.noreply.github.com>
2022-06-26 19:48:27 +0300

    Update 8byte.py

--- 8byte.py
+++ 8byte.py
@@ -37,27 +37,27 @@ for i in A:
     while deger < 5:
         if A[deger] == 0:
             liste_0.append(A[deger])
-            color = '#808090'
+           
         elif A[deger] ==1 :
             liste_1.append(A[deger])
-            color = '#808085'
+           
         elif A[deger] == 2:
-            color = '#808080'
+            
             liste_2.append(A[deger])
         elif A[deger] ==3 :
-            color = '#808075'
+            
             liste_3.append(A[deger]) 
         elif A[deger] ==4 :
-            color = '#808075'
+            
             liste_4.append(A[deger])
         elif A[deger] ==5 :
-            color = '#808075'
+            
             liste_5.append(A[deger]) 
         elif A[deger] ==6:
-            color = '#808075'
+            
             liste_6.append(A[deger])
         elif A[deger] ==7 :
-            color = '#808075'
+            
             liste_7.append(A[deger]) 
         deger += 1
 deger = 0
@@ -66,27 +66,27 @@ for i in B:
     while deger < 5:
         if B[deger] == 0:
             liste_0.append(B[deger])
-            color = '#808090'
+            
         elif B[deger] ==1 :
             liste_1.append(B[deger])
-            color = '#808085'
+            
         elif B[deger] == 2:
-            color = '#808080'
+        
             liste_2.append(B[deger])
         elif B[deger] ==3 :
-            color = '#808075'
+            
             liste_3.append(B[deger]) 
         elif B[deger] ==4 :
-            color = '#808075'
+            
             liste_4.append(B[deger])
         elif B[deger] ==5 :
-            color = '#808075'
+         
             liste_5.append(B[deger]) 
         elif B[deger] ==6:
-            color = '#808075'
+            
             liste_6.append(B[deger])
         elif B[deger] ==7 :
-            color = '#808075'
+            
             liste_7.append(B[deger]) 
         deger += 1
 deger = 0
@@ -95,27 +95,27 @@ for i in C:
     while deger < 5:
         if C[deger] == 0:
             liste_0.append(C[deger])
-            color = '#808090'
+            
         elif C[deger] ==1 :
             liste_1.append(C[deger])
-            color = '#808085'
+            
         elif C[deger] == 2:
-            color = '#808080'
+            
             liste_2.append(C[deger])
         elif C[deger] ==3 :
-            color = '#808075'
+            
             liste_3.append(C[deger]) 
         elif C[deger] ==4 :
-            color = '#808075'
+           
             liste_4.append(C[deger])
         elif C[deger] ==5 :
-            color = '#808075'
+           
             liste_5.append(C[deger]) 
         elif C[deger] ==6:
-            color = '#808075'
+         
             liste_6.append(C[deger])
         elif C[deger] ==7 :
-            color = '#808075'
+            
             liste_7.append(C[deger]) 
         deger += 1
 deger = 0
@@ -123,27 +123,27 @@ for i in D:
     while deger < 5:
         if D[deger] == 0:
             liste_0.append(D[deger])
-            color = '#808090'
+            
         elif D[deger] ==1 :
             liste_1.append(D[deger])
-            color = '#808085'
+            
         elif D[deger] == 2:
-            color = '#808080'
+       
             liste_2.append(D[deger])
         elif D[deger] ==3 :
-            color = '#808075'
+         
             liste_3.append(D[deger]) 
         elif D[deger] ==4 :
-            color = '#808075'
+         
             liste_4.append(D[deger])
         elif D[deger] ==5 :
-            color = '#808075'
+         
             liste_5.append(D[deger]) 
         elif D[deger] ==6:
-            color = '#808075'
+        
             liste_6.append(D[deger])
         elif D[deger] ==7 :
-            color = '#808075'
+        
             liste_7.append(D[deger]) 
         deger += 1
 deger = 0
@@ -153,27 +153,27 @@ for i in E:
     while deger < 5:
         if E[deger] == 0:
             liste_0.append(E[deger])
-            color = '#808090'
+        
         elif E[deger] ==1 :
             liste_1.append(E[deger])
-            color = '#808085'
+           
         elif E[deger] == 2:
-            color = '#808080'
+     
             liste_2.append(E[deger])
         elif E[deger] ==3 :
-            color = '#808075'
+          
             liste_3.append(E[deger]) 
         elif E[deger] ==4 :
-            color = '#808075'
+           
             liste_4.append(E[deger])
         elif E[deger] ==5 :
-            color = '#808075'
+           
             liste_5.append(E[deger]) 
         elif E[deger] ==6:
-            color = '#808075'
+            
             liste_6.append(E[deger])
         elif E[deger] ==7 :
-            color = '#808075'
+           
             liste_7.append(E[deger]) 
         deger += 1
 deger = 0
